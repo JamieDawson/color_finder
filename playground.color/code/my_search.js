91a01332900695e44d6ac0c4f14681035764753e
JamieDawson <jamierobertdawson@gmail.com>
2019-08-09 22:20:05 -0700

    currently broken. Got to fix the url in the json and update the result_view

--- playground.color/code/my_search.js
+++ playground.color/code/my_search.js
@@ -1,9 +1,17 @@
 module.exports.function = function my_search (color_one, color_two) {
   
   var test = "not found"
+  var result = []
+  var all = {}
   
-  if (color_one == "red" && color_two == "blue")
-    test = "found it!!!"
+  if (color_one == "red" && color_two == "blue") {
+    all = {
+      combined_color: "GREEN BITCH",
+      image_color: "/images/green.png"
+    }
+    result.push(all)
+    console.log(result)
+  }
   
-  return test
+  return all
 }
